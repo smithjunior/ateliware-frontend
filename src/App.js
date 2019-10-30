efda417709bb7c81237ee8244adcaa0d2ed567df
Smith Júnior <smith.junior@icloud.com>
2019-10-30 01:51:02 -0300

    Added ateliware logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Routes from './routes'
-// import logo from './logo.svg'
+import logo from './logo.svg'
 import './App.css'
 
 function App () {
   return (
     <div className='container'>
-      {/* <img src={logo} alt='Ateliware' /> */}
+      <img src={logo} className='logo' alt='Ateliware' />
       <div className='content'>
         <Routes />
       </div>
